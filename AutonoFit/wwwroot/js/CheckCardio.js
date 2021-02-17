d7ad6434f4e3a1f5728f9aff62ea189bc080bd9b
GRy82 <gwryan@buffalo.edu>
2021-02-16 22:09:02 -0500

    Same logic added to ProgramSetup view. Scripts extracted from views and placed into their own .js files.

---
+++ AutonoFit/wwwroot/js/CheckCardio.js
@@ -0,0 +1,10 @@
+﻿function checkCardio(goalOneValue, goalTwoValue) {
+    if (goalOneValue == 4 || goalOneValue == 5 || goalTwoValue == 4 || goalTwoValue == 5) {
+        document.getElementById("mile-min").style.display = "inline";
+        document.getElementById("mile-sec").style.display = "inline-block";
+    }
+    else {
+        document.getElementById("mile-min").style.display = "none";
+        document.getElementById("mile-sec").style.display = "none";
+    }
+}
---
+++ AutonoFit/wwwroot/js/CheckCardioAndSuitableGoal.js
@@ -0,0 +1,14 @@
+﻿function checkCardio(goalOneValue, goalTwoValue) {
+    if (goalOneValue == 4 || goalOneValue == 5 || goalTwoValue == 4 || goalTwoValue == 5) {
+        document.getElementById("mile-min").style.display = "inline";
+        document.getElementById("mile-sec").style.display = "inline-block";
+    }
+    else {
+        document.getElementById("mile-min").style.display = "none";
+        document.getElementById("mile-sec").style.display = "none";
+    }
+    if ((goalOneValue == 1 || goalOneValue == 2 || goalTwoValue == 1 || goalTwoValue == 2)) {
+        alert("Because you do not own equipment conducive to heavier lifting exercises, it is unlikely that the workout generated will"
+            + " be suitable towards your goals if those goals include strength or hypertrophy.");
+    }
+}
