3157b16712150521d0cd2130a00d3194004bf971
GRy82 <gwryan@buffalo.edu>
2021-04-10 12:57:57 -0400

    ClientExercise Model removed from project. Db will need to be updated. The fields needed form ClientExercise were distributed to the recently added Exercise model.  Repository and COntracts updated to reflect these changes.

---
+++ AutonoFit/Contracts/IExerciseRepository.cs
@@ -0,0 +1,19 @@
+﻿using AutonoFit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutonoFit.Contracts
+{
+    public interface IExerciseRepository : IRepositoryBase<Exercise>
+    {
+        void CreateExercise(Exercise exercise);
+        Task<List<Exercise>> GetAllExercisesAsync(int clientId);
+        Task<List<Exercise>> GetExerciseAsync(int exerciseId);
+        Task<List<Exercise>> GetExerciseByWorkoutAsync(int workoutId);
+        Task<List<Exercise>> GetExercisesByProgramAsync(int programId, int exerciseId);
+        void EditExercise(Exercise exercise);
+        void DeleteExercise(Exercise exercise);
+    }
+}
---
+++ AutonoFit/Data/Repositories/ExerciseRepository.cs
@@ -0,0 +1,51 @@
+﻿using AutonoFit.Contracts;
+using AutonoFit.Models;
+using AutonoFit.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutonoFit.Data.Repositories
+{
+    public class ExerciseRepository : RepositoryBase<Exercise>, IExerciseRepository
+    {
+        private readonly object _repo;
+
+        public ExerciseRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+
+        }
+
+        public void CreateExercise(Exercise clientExercise) => Create(clientExercise);
+
+        public async Task<List<Exercise>> GetAllExercisesAsync(int clientId) =>
+            await FindByCondition(c => c.ClientId.Equals(clientId)).ToListAsync();
+
+        public async Task<List<Exercise>> GetExerciseAsync(int exerciseId) =>
+            await FindByCondition(c => c.id.Equals(exerciseId)).ToListAsync();
+
+        public async Task<List<Exercise>> GetExerciseByWorkoutAsync(int workoutId) =>
+            await FindByCondition(c => c.WorkoutId.Equals(workoutId)).ToListAsync();
+
+        public async Task<List<Exercise>> GetExercisesByProgramAsync(int programId, int exerciseId)
+        {
+            List<Exercise> programExercises = await FindByCondition(c => c.ProgramId.Equals(programId)).ToListAsync();
+            List<Exercise> exercises = new List<Exercise> { };
+            foreach (Exercise exercise in programExercises)
+            {
+                if (exercise.id == exerciseId)
+                {
+                    exercises.Add(exercise);
+                }
+            }
+            return exercises;
+        }
+
+        public void EditExercise(Exercise exercise) => Update(exercise);
+        public void DeleteExercise(Exercise exercise) => Delete(exercise);
+
+
+    }
+}
